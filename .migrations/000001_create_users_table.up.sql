d22271bcf6ab13e699c409812b54796f4bd16a5d
Maksim Bondarenko <maksimkab2011@gmail.com>
2026-03-28 11:20:48 +0200

    init commit

---
+++ .migrations/000001_create_users_table.up.sql
@@ -0,0 +1,16 @@
+-- Active: 1774086832819@@127.0.0.1@5432@ecommerce_db
+CREATE TABLE IF NOT EXISTS users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'customer',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        CHECK (role IN ('customer', 'admin')),
+    create_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    update_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE INDEX idx_users_email ON user(email);
+CREATE INDEX idx_users_role ON user(role);
